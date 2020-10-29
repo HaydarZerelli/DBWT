f695b255098666a4ac8026fdc0dff7149ab6190d
HaydarZerelli <Haydar.Zerelli@github.com>
2020-10-29 15:28:47 +0100

    Aufgabe 3 : 1, 2 , 3(a,b) fertig

--- beispiele/meal.php
+++ beispiele/meal.php
@@ -2,7 +2,8 @@
 const GET_PARAM_MIN_STARS = 'search_min_stars';
 const GET_PARAM_SEARCH_TEXT = 'search_text';
 const GET_PARAM_AUTHOR = 'search_author';//Eingefügt um der Author zu suchen.
-const GET_PARAM_SEARCH_ALLERGENE='search_allergene';//Allergene Suchen
+
+
 /**
  * Liste aller möglichen Allergene.
  */
@@ -13,6 +14,7 @@ $allergens = array(
     14 => 'Fisch',
     17 => 'Milch')
 ;
+
 $meal = [ // Kurzschreibweise für ein Array (entspricht = array())
     'name' => 'Süßkartoffeltaschen mit Frischkäse und Kräutern gefüllt',
     'description' => 'Die Süßkartoffeln werden vorsichtig aufgeschnitten und der Frischkäse eingefüllt.',
@@ -21,6 +23,7 @@ $meal = [ // Kurzschreibweise für ein Array (entspricht = array())
     'allergens' => [11, 13],
     'amount' => 42   // Anzahl der verfügbaren Gerichte.
 ];
+
 $ratings = [
     [   'text' => 'Die Kartoffel ist einfach klasse. Nur die Fischstäbchen schmecken nach Käse. ',
         'author' => 'Ute U.',
@@ -35,17 +38,6 @@ $ratings = [
         'author' => 'Marta M.',
         'stars' => 3 ]
 ];
-$showAllergene=[];
-if(!empty($_GET[GET_PARAM_SEARCH_ALLERGENE])){
-    $searchTerm = $_GET[GET_PARAM_SEARCH_ALLERGENE];
-    foreach ($allergens as $allergens){
-        if($allergens == $searchTerm){
-            $showAllergene[]=$allergens;
-        }
-    }
-}else {
-    $showAllergene = $allergens;
-}
 $showRatings = [];
 if (!empty($_GET[GET_PARAM_SEARCH_TEXT])) {
     $searchTerm = $_GET[GET_PARAM_SEARCH_TEXT];
@@ -62,7 +54,7 @@ if (!empty($_GET[GET_PARAM_SEARCH_TEXT])) {
         }
     }
     /*Diese else if habe eingefügt für die Autoren zu suchen*/
- }else if (!empty($_GET[GET_PARAM_AUTHOR])){
+}else if (!empty($_GET[GET_PARAM_AUTHOR])){
     $searchAuthor = $_GET[GET_PARAM_AUTHOR];
     foreach ($ratings as $rating) {
         if (strpos($rating['author'], $searchAuthor) !== false) {
@@ -83,56 +75,60 @@ function calcMeanStars($ratings) : float { // : float gibt an, dass der Rückgab
     return $sum / count($ratings);
 }
 
+$showAllergene=[];
+foreach ($meal['allergens'] as $allerg){
+    $showAllergene[]=$allergens["$allerg"];
+}
 ?>
 <!DOCTYPE html>
 <html lang="de">
-    <head>
-        <meta charset="UTF-8"/>
-        <title>Gericht: <?php echo $meal['name']; ?></title>
-        <style type="text/css">
-            * {
-                font-family: Arial, serif;
-            }
-            .rating {
-                color: darkgray;
-            }
-        </style>
-    </head>
-    <body>
-        <h1>Gericht: <?php echo $meal['name']; ?></h1>
-        <p><?php echo $meal['description']; ?></p>
-        <h1>Bewertungen (Insgesamt: <?php echo calcMeanStars($ratings); ?>)</h1>
-        <form method="get">
-            <label for="search_text">Filter:</label>
-            <input id="search_text" type="text" name="search_text">
-            <input type="submit" value="Suchen">
-        </form>
-        <table class="rating">
-            <thead>
-            <tr>
-                <td>Text</td>
-                <td>Sterne</td>
-                <td>Author</td>
-            </tr>
-            </thead>
-            <tbody>
-            <?php
-        foreach ($showRatings as $a) {
-            echo "<tr><td class='rating_text'>{$rating['text']}</td>
+<head>
+    <meta charset="UTF-8"/>
+    <title>Gericht: <?php echo $meal['name']; ?></title>
+    <style type="text/css">
+        * {
+            font-family: Arial, serif;
+        }
+        .rating {
+            color: darkgray;
+        }
+    </style>
+</head>
+<body>
+<h1>Gericht: <?php echo $meal['name']; ?></h1>
+<p><?php echo $meal['description']; ?></p>
+<h1>Bewertungen (Insgesamt: <?php echo calcMeanStars($ratings); ?>)</h1>
+<form method="get">
+    <label for="search_text">Filter:</label>
+    <input id="search_text" type="text" name="search_text"  >
+    <input type="submit" value="Suchen">
+</form>
+<table class="rating">
+    <thead>
+    <tr>
+        <td>Text</td>
+        <td>Sterne</td>
+        <td>Author</td>
+    </tr>
+    </thead>
+    <tbody>
+    <?php
+    foreach ($showRatings as $rating) {
+        echo "<tr><td class='rating_text'>{$rating['text']}</td>
                       <td class='rating_stars'>{$rating['stars']}</td>
                       <td class='rating_authors'>{$rating['author']}</td>/*das ist das Table*/
                   </tr>";
-        }
-        ?>
-            </tbody>
-        </table>
-       /*die Liste für die Allergene zum Gericht*/
-    <ul class="allergene">
-        <?php
-        foreach ($showAllergene as $allergens ) {
-            <li></li>
-        }
-         ?>
-    </ul>
-    </body>
+    }
+    ?>
+    </tbody>
+</table>
+<!--        Liste von Allergene Ausgeben -->
+<ul class="allerg">
+    <?php
+    foreach($showAllergene as $allerg ){
+        echo " <li>{$allerg}</li>";
+    }
+    ?>
+</ul>
+</body>
 </html>
