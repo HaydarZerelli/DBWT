f91905f6dba46e81f0252e56b22cc33822ab5681
Philipp_Hünnerscheidt <philipp.huennerscheidt@yahoo.de>
2020-11-30 14:39:38 +0100

    M4 Aufgabe 6 angefangen, 6a fertig

--- emensa/controllers/ExampleController.php
+++ emensa/controllers/ExampleController.php
@@ -4,6 +4,7 @@ require_once('../models/kategorie.php');
 class ExampleController
 {
     public function m4_6a_queryparameter(RequestData $rd) {
-        // ...
+        $vars = ['name' => $rd->query['name'] ?? 'name'];
+        return view('examples.m4_6a_queryparameter', $vars);
     }
 }
---
+++ emensa/views/examples/m4_6a_queryparameter.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <title>6a_queryparameter</title>
+</head>
+<body>
+<p>Der Wert von ?name lautet: {{$name}}</p>
+</body>
+</html>
+
